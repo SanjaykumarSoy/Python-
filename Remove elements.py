e27dc2f94ef889fa4fd2ae64683363fa90dfdb8d
SanjaykumarSoy <157505017+SanjaykumarSoy@users.noreply.github.com>
2024-05-02 19:43:19 +0530

    Create Remove elements.py

---
+++ Remove elements.py
@@ -0,0 +1,16 @@
+def remove_element(nums, val):
+    
+  
+    i = 0
+    n = len(nums)
+    
+    
+    while i < n:
+      
+        if nums[i] == val:
+            nums[i] = nums[n - 1]
+            n -= 1
+        else:
+            i += 1
+  
+    return n
